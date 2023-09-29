5d7bea1b4b7ea0a88ef975030431384b6462a66e
michelle15cui <cuimi@ssfs.org>
2023-09-28 22:37:30 -0400

    michelle

---
+++ degrees.py
@@ -0,0 +1,3 @@
+celcius_Temperature=input("What is the temperature in Celcius?")
+fahrenheit=1.8*int(celcius_Temperature)+32
+print(celcius_Temperature, "degrees for Celcius in Fahrenheit is", fahrenheit, "degrees")
---
+++ variable.py
@@ -0,0 +1,6 @@
+p = 10000
+n = 12
+r = 0.08
+time = 10
+final_amount = p * (1 + r / n) ** (n * time)
+print("The amount of compound interest is", final_amount)
